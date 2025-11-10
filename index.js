aa657e13d01e1baa5d1b30a26f5ed9b69923efd7
mrchipsmaruf <mrchipsmaruf@gmail.com>
2025-11-10 21:12:56 +0600

    added all foods api on the server site

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-let express = require ('express');
-let cors = require ('cors');
+let express = require('express');
+let cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+let app = express();
+let port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://PlateShareDB:zTBqUWgbGU3BppYO@cluster0.rjffgqf.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Smart server is running');
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        let db = client.db('PlateShareDB')
+        let foodsCollections = db.collection('foods');
+
+        app.get('/foods', async(req, res) => {
+            let result = await foodsCollections.find().toArray();
+            res.send(result);
+        })
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+    }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Smart server is running on port: ${port}`);
+});
